b732d26ea7809f0ebd1b0b989f8269e919c107bf
Juan Felipe Garzon Baron <garzonbaronj@gmail.com>
2026-05-04 11:47:29 -0500

    LAB 9.1

--- movie.py
+++ movie.py
@@ -1,4 +1,14 @@
-# Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+
+    def get_length_in_seconds(self):
+        return self.length * 60
+
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} ({self.length})"
 # FREEZE CODE BEGIN
 class Movie:
     def __init__(self, title, director, year):
