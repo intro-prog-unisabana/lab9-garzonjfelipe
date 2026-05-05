a9b147c87eaa52272dd4f9015e98d8870baa63e9
Juan Felipe Garzon Baron <garzonbaronj@gmail.com>
2026-05-04 19:48:15 -0500

    lab 9.6

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-def main():
-    people = []  # List to store all Person objects
+from utils import person_data, balance_summary
+from bank_account import BankAccount
 
-    while True:
-        # Display menu
-        print("Choose an option:")
-        print("1. Add a new person")
-        print("2. Add an account to a person")
-        print("3. Show all balances")
-        print("4. Quit")
+people = []
 
-        choice = input().strip()
+while True:
+    print("Choose an option:")
+    print("1. Add a new person")
+    print("2. Add an account to a person")
+    print("3. Show all balances")
+    print("4. Quit")
 
-        # Option 1: Add a new person
-        if choice == "1":
-            pass
+    option = input()
 
-        # Option 2: Add an account to an existing person
-        elif choice == "2":
-            pass
+    if option == "1":
+        person = person_data()
+        people.append(person)
 
-        # Option 3: Show all balances
-        elif choice == "3":
-            pass
+    elif option == "2":
+        name = input("Enter the person's name:")
+        found = False
 
-        # Option 4: Quit
-        elif choice == "4":
-            print("Goodbye!")
-            break
+        for person in people:
+            if person.name == name:
+                acc_num = int(input("Enter a 4-digit account number:"))
+                balance = float(input("Enter the initial balance:"))
+                person.add_account(BankAccount(acc_num, balance))
+                found = True
+                break
 
-        # Invalid input
+        if not found:
+            print("Person not found.")
+
+    elif option == "3":
+        if len(people) == 0:
+            print("No data to show.")
         else:
-            print("Invalid option. Please choose 1-4.")
+            balance_summary(people)
 
-if __name__ == "__main__":
-    main()
-
+    elif option == "4":
+        print("Goodbye!")
+        break
